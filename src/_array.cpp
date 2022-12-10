222572058eac781b510d655533bc1d6cc1d8e85a
soulrrrrr <chialun100@gmail.com>
2022-12-10 14:08:34 +0800

    add multi dimensional array and basic operation

--- src/_array.cpp
+++ src/_array.cpp
@@ -3,40 +3,145 @@
 #include <utility>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-
+#include <pybind11/operators.h>
 using std::vector;
 
 namespace py = pybind11;
 
+void check_dim(vector<size_t> v1, vector<size_t> v2)
+{
+    if (v1 != v2)
+        throw std::range_error("the dimension of 2 array should be same");
+}
+
+
 class Array {
 public:
-    Array(){}
+    Array(): m_size(0) {}
     Array(vector<int> v): m_data(v), m_size(v.size()), m_dim(vector<size_t>(1, v.size())) {}
+    Array(vector<int> v, vector<size_t> dim): m_data(v), m_size(v.size()), m_dim(dim) {}
+
+    Array operator+(Array v)
+    {
+        check_dim(m_dim, v.m_dim);
+        Array ret(m_data, m_dim);
+        for (int i = 0; i < m_size; i++)
+            ret.m_data[i] += v.m_data[i];
+        return ret;
+    }
+
+    Array operator-(Array v)
+    {
+        check_dim(m_dim, v.m_dim);
+        Array ret(m_data, m_dim);
+        for (int i = 0; i < m_size; i++)
+            ret.m_data[i] -= v.m_data[i];
+        return ret;
+    }
+
+    Array operator*(Array v)
+    {
+        check_dim(m_dim, v.m_dim);
+        Array ret(m_data, m_dim);
+        for (int i = 0; i < m_size; i++)
+            ret.m_data[i] *= v.m_data[i];
+        return ret;
+    }
+
+    bool operator==(Array& v)
+    {
+        if (m_size != v.m_size)
+            return false;
+        if (m_dim != v.m_dim)
+            return false;
+        if (m_data != v.m_data)
+            return false;
+        return true;
+    }
+
+    Array arange(const vector<size_t>& v)
+    {
+        size_t size = 1;
+        for (auto i : v)
+            size *= i;
+        if (m_size != size)
+            throw std::range_error("the size of 2 arrays are not same");
+
+        m_dim.clear();
+        for (auto i : v)
+            m_dim.push_back(i);
+
+        return *this;
+    }
+
 public:
     size_t m_size;
     vector<size_t> m_dim;
     vector<int> m_data;
 };
 
+Array zeros(vector<size_t> dim)
+{
+    size_t size = 1;
+    for (size_t d : dim)
+        size *= d;
+    return Array(vector<int>(size, 0), dim);
+}
+
+Array ones(vector<size_t> dim)
+{
+    size_t size = 1;
+    for (size_t d : dim)
+        size *= d;
+    return Array(vector<int>(size, 1), dim);
+}
+
 PYBIND11_MODULE(_array, m) {
     py::class_<Array>(m, "Array")
         .def(py::init<vector<int>>())
         .def("__getitem__", [](Array &self, size_t idx) {
+            if (self.m_size <= idx)
+                throw std::range_error("index too large");
             return self.m_data[idx];
         })
         .def("__getitem__", [](Array &self, vector<size_t> idx) {
-            return self.m_data[idx[0]];
+            if (self.m_dim.size() != idx.size())
+                throw std::range_error("the dimension of query indexes should be as same as the dimension of the array");
+            size_t real_idx = 0;
+            for (int i = idx.size()-1; i >= 0; i--)
+            {
+                real_idx *= self.m_dim[i];
+                real_idx += idx[i];
+            }
+            return self.m_data[real_idx];
         })
         .def("__setitem__", [](Array &self, size_t idx, const int value) {
+            if (self.m_size <= idx)
+                throw std::range_error("index too large");
             self.m_data[idx] = value;
         })
         .def("__setitem__", [](Array &self, vector<size_t> idx, const int value) {
-            self.m_data[idx[0]] = value;
+            if (self.m_dim.size() != idx.size())
+                throw std::range_error("the dimension of query indexes should be as same as the dimension of the array");
+            size_t real_idx = 0;
+            for (int i = idx.size()-1; i >= 0; i--)
+            {
+                real_idx *= self.m_dim[i];
+                real_idx += idx[i];
+            }
+            self.m_data[real_idx] = value;
         })
         .def("show", [](Array &self){
             for (auto i : self.m_data)
                 py::print(i);
         })
+        .def("arange", &Array::arange)
+        .def("__add__", [](Array &a1, Array &a2){ return a1 + a2;})
+        .def("__sub__", [](Array &a1, Array &a2){ return a1 - a2;})
+        .def("__mul__", [](Array &a1, Array &a2){ return a1 * a2;})
+        .def("__eq__", [](Array &a1, Array &a2){ return a1 == a2;})
         .def_readonly("size", &Array::m_size);
+    m.def("zeros", &zeros);
+    m.def("ones", &ones);
 
 }
--- src/test_array.py
+++ src/test_array.py
@@ -1,6 +1,48 @@
 import pytest
 import _array
+import numpy as np
 
 def test_init():
     a = _array.Array([1, 2, 4, 5])
     assert a.size == 4
+
+def test_zeros():
+    a = _array.Array([1, 2, 4, 5])
+    b = _array.zeros([3, 3])
+    assert b.size == 9
+
+def test_get():
+    a = _array.Array([1, 2, 4, 5])
+    b = _array.zeros([3, 3])
+    assert b[0] == 0
+    assert b[0, 0] == 0
+    with pytest.raises(ValueError):
+        b[0, 0, 0] = 0
+
+def test_set():
+    a = _array.Array([1, 2, 4, 5])
+    b = _array.zeros([3, 3])
+    assert a[0] == 1
+    a[0] = 3
+    assert a[0] == 3
+    assert b[1, 2] == 0
+    b[5] = 7
+    assert b[5] == 7
+
+def test_add():
+    a = _array.Array([i for i in range(9)]).arange([3, 3])
+    b = _array.ones([3, 3])
+    c = _array.Array([i for i in range(1, 10)]).arange([3, 3])
+    assert a + b == c
+
+def test_sub():
+    a = _array.Array([i for i in range(1, 10)]).arange([3, 3])
+    b = _array.ones([3, 3])
+    c = _array.Array([i for i in range(9)]).arange([3, 3])
+    assert a - b == c
+
+def test_mul():
+    a = _array.Array([i for i in range(1, 10)]).arange([3, 3])
+    b = _array.Array([i for i in range(1, 10)]).arange([3, 3])
+    c = _array.Array([i*i for i in range(1, 10)]).arange([3, 3])
+    assert a * b == c
